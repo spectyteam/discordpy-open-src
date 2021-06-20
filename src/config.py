a774f998ff75900b58610ab5df4e434fede5c090
mesiik <71527914+mesiik@users.noreply.github.com>
2021-06-20 08:38:35 +0200

    🤔v2

---
+++ src/config.py
@@ -0,0 +1,12 @@
+#Basic
+TOKEN = "" #Bot token
+
+PREFIX = ""#Bot prefix
+
+OWNER = 12345 #Your id
+
+LOGS = 12345 #Private channel id, this will log new guilds, direct messages with bot etc.
+
+#Emojis 
+CHECKMARK = "" #(✅ emoji)
+CROSSMARK = "" #(❌ emoji)
